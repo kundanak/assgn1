e266de5fcea019753871012e1c2f0ccde02f15b6
kundanak <kundana.sree14@gmail.com>
2017-01-25 19:00:01 -0500

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,56 @@
+package bean;
+
+public class Register {
+	
+
+     private int id;
+     private string firstName;
+     private string LastName;
+     private string email;
+     private string Address;
+     
+     public Regsiter() {
+	
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public string getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(string firstName) {
+		this.firstName = firstName;
+	}
+
+	public string getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(string lastName) {
+		LastName = lastName;
+	}
+
+	public string getEmail() {
+		return email;
+	}
+
+	public void setEmail(string email) {
+		this.email = email;
+	}
+
+	public string getAddress() {
+		return Address;
+	}
+
+	public void setAddress(string address) {
+		Address = address;
+	}
+
+
